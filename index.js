289eee65fc5ff18c8dfc10648ecc3a60f9dbbcd5
Agentrapyntsel <shinkovich79@gmail.com>
2022-01-15 15:46:10 +0300

    seo bugs fixed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-document.addEventListener('touchstart', onTouchStart, {passive: true});
+/* Feature detection */
+var passiveSupported = false;
+try {
+	window.addEventListener(
+		"test",
+		null,
+		Object.defineProperty({}, "passive", { get: function () { passiveSupported = true; } }));
+} catch (err) { }
+
+/* Event Listener */
+var elem = document.getElementById('footer');
+elem.addEventListener(
+	'touchmove',
+	function listener() {   /* do something */ },
+	passiveSupported ? { passive: true } : false
+);
