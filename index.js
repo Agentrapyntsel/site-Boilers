0e1df235127df07008c72b133ae192e067547c40
Agentrapyntsel <shinkovich79@gmail.com>
2022-01-23 22:49:26 +0300

    animation added

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 /* Feature detection */
-var passiveSupported = false;
-try {
-	window.addEventListener(
-		"test",
-		null,
-		Object.defineProperty({}, "passive", { get: function () { passiveSupported = true; } }));
-} catch (err) { }
+// var passiveSupported = false;
+// try {
+// 	window.addEventListener(
+// 		"test",
+// 		null,
+// 		Object.defineProperty({}, "passive", { get: function () { passiveSupported = true; } }));
+// } catch (err) { }
 
-/* Event Listener */
-var elem = document.getElementById('footer');
-elem.addEventListener(
-	'touchmove',
-	function listener() {   /* do something */ },
-	passiveSupported ? { passive: true } : false
-);
+// /* Event Listener */
+// var elem = document.getElementById('footer');
+// elem.addEventListener(
+// 	'touchmove',
+// 	function listener() {   /* do something */ },
+// 	passiveSupported ? { passive: true } : false
+// );
